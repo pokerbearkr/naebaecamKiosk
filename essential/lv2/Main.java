f27b88d492eab48991a3f0e1131857dfd9cc76aa
pokerbearkr <ogdongwon@gmail.com>
2025-03-11 09:44:35 +0900

    docs : 필수 LV2 주석 추가

--- essential/lv2/Main.java
+++ essential/lv2/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        List<MenuItem> itemList = new ArrayList<>();
+        List<MenuItem> itemList = new ArrayList<>(); //메뉴를 담을 리스트 생성
         itemList.add(new MenuItem("ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
         itemList.add(new MenuItem("SmokeShack", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
         itemList.add(new MenuItem("Cheeseburger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
--- essential/lv2/MenuItem.java
+++ essential/lv2/MenuItem.java
@@ -1,16 +1,19 @@
 package essential.lv2;
 
+// 아이템들을 담는 클래스
 public class MenuItem {
-    String name;
-    int price;
-    String description;
+    String name; // 메뉴 이름
+    int price; // 메뉴 가격
+    String description; // 메뉴 설명
 
+    // 생성자
     public MenuItem(String name, int price, String description) {
         this.name = name;
         this.price = price;
         this.description = description;
     }
 
+    // getter로 받아옴
     public String getName() {
         return name;
     }
